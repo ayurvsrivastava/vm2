91a2ec4ee5ae75c4af419789c836556d8e80229e
Ayurv Srivastava <ayurv@Ayurvs-MacBook-Pro.local>
2022-09-20 01:02:00 -0400

    requirements and setup

---
+++ rushee.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+from typing import List
+
+class Rushee(BaseModel):
+    name: str
+    rank: int
+    app_score: float
+    gi_score: float
+    ii_score: str
+    bhs_score: int
