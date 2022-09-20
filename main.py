dac82e56b41549e3e4021d459fe170b4dde0ebd1
Ayurv Srivastava <ayurv@Ayurvs-MacBook-Pro.local>
2022-09-20 01:11:39 -0400

    update requirements.txt

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
+import pandas
 app = Flask(__name__)
 
 
 @app.route('/', methods=["POST"])
 def index():
-    
+    pass
 
 
 if __name__ == '__main__':
